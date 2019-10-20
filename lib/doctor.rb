c7f92db14f3ba016d4334aad46f13baf6993250a
David Bermudez <juan.bermudez1102@outlook.com>
2019-10-20 14:00:43 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -7,4 +7,14 @@ class Doctor
     @name = name
     @@all << self
   end
+
+  def appointments
+    Appointment.all.select {|appointment|
+      appointment.doctor == self
+    }
+  end
+
+  def new_appointment(date,patient)
+    Appointment.new(date,patient,self)
+  end
 end
